700c9befac3b0c015e75f97f45ef9714913a4e61
saldhyyoga <saldhyy@gmail.com>
2019-10-29 20:39:30 +0700

    Updates

---
+++ precache-manifest.8cbb0874f99011eea60634e7ee8f66a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff67c5e31a2bf76498ac8b58090a629a",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "ebc637364fc438252f73",
+    "url": "/first-react/static/css/main.d6da11d1.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/first-react/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "ebc637364fc438252f73",
+    "url": "/first-react/static/js/main.41dc9401.chunk.js"
+  },
+  {
+    "revision": "c341207d301dab572c5e",
+    "url": "/first-react/static/js/runtime~main.819d487f.js"
+  }
+]);
